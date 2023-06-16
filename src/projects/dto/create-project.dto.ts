d78faa4034f5d71d54fdabe0a57ed574af8528e3
jurayevcoder <davlatjurayev.1@gmail.com>
2023-06-16 17:04:42 +0500

    change

--- src/projects/dto/create-project.dto.ts
+++ src/projects/dto/create-project.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty, IsString } from "class-validator";
+import { IsArray, IsNotEmpty, IsObject, IsString } from "class-validator";
 
 export class CreateProjectDto {
     @ApiProperty({example: "GitHub", description: "Project Name"})
